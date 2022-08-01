b6319a5dd1240dbc9defb5945d9746d5a41fa023
StanimirResh <mchorko@gmail.com>
2022-08-01 20:06:03 +0300

    Added router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { Header } from "./components/common/Header";
 import { Main } from "./components/common/Main";
+import { Routes, Route } from 'react-router-dom'
 function App() {
   return (
     <div id="box">
       <Header />
-      <Main />
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
     </div>
   );
 }
