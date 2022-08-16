1eca9c15c3cc30646553671c95daa41a98fcedf8
StanimirResh <mchorko@gmail.com>
2022-08-16 15:58:41 +0300

    created service for all reqs to the server

--- src/services/gameService.js
+++ src/services/gameService.js
@@ -1,7 +1,6 @@
-const baseUrl = "http://localhost:3030/"
+import * as request from './requester'
 
-export const getAll = () => {
-    return fetch(`${baseUrl}data/games?sortBy=_createdOn%20desc&distinct=category`)
-        .then(res => res.json())
-        
-}
+
+const baseUrl = "http://localhost:3030/data/games"
+
+export const getAll = () => request.get(baseUrl)
